f942a0a7d59e2f7a6d04d25601dc4cb6283fcf14
Oleksandr <olexandr.veseluy@gmail.com>
2025-02-27 15:22:27 +0200

    initial project

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+
+import "./globals.css";
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Spika",
+  description: "Spika — Your Speaking Buddy",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
